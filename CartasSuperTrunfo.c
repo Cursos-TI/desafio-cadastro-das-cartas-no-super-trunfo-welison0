54638c4d0b6930630cd9634d025c30354a849a1b
welison0 <welisoncarlos123@gmail.com>
2025-08-01 23:51:59 +0000

    iniciei o programa usando "#include <stdio.h>" que e a biblioteca padrão do C. em seguida inseri a parte principal do C "int main()[. inseri as variaveis onde cada variavel gurda suas informaçoes. em seguida parti para utilização do (printf) para inserir o titulo "Carta1!) em seguida comecei a utilização das ferrmenta indicadas para tarefa pelos tutores (printf e scanf), printf=mostra a pergunta, ja o scanf=ler o que foi digitado, assim o programa reconhece o que ele tem que perguntar e tambem reconhece o que foi digitado pelo usuario. quase no fim entrei na parte de impressão para mostrar as informaçoes que foram armazenadas com(printf)  e inseri o (return 0;) indicando que o programa terminou.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -32,7 +32,7 @@ printf("Digite o Estado: ");
     printf("Digite Numero de Pontos turisticos: ");
     scanf("%d", &Ponto);
 
-    printf("\n--- INFORMAÇÕES DIGITADAS ---\n");
+   
     printf("Estado: %s\n", Estado);
     printf("Cidade: %s\n", Cidade);
     printf("Codigo: %s\n", Codigo);
