b2f67e09baa6463f4faf2ca338563a9278248293
welison0 <welisoncarlos123@gmail.com>
2025-08-18 23:39:20 +0000

    variaveis de decissão if_else adicionadas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    //variavel carta 1
+    //variavel carta 1             //variaveis 1 adicionadas
     char Estado[15] ;
     char Codigo[20];
     char Cidade[20];
@@ -11,7 +11,7 @@ int main() {
 
      //obs:adicioneis asvariaveis float antes de receber o resultado, sempre dava erro e nao sabia o porque.
 
-    //variavel carta2
+    //variavel carta2                 //variavel carta 2 adicionada
     char Estado2[20];
     char Codigo2[10];
     char Cidade2[20];
@@ -19,7 +19,7 @@ int main() {
     float Area2, PIB2;
     int Ponto2;
 
-   //nivel mestre, variaveis adicionadas
+   //nivel mestre, variaveis adicionadas                  //novas variaveis adicionadas
    float densidadePopulacao, densidadePopulacao2;
    float pibTotal, pibTotal2;
    float pibpercapita, pibpercapita2;
@@ -28,6 +28,7 @@ int main() {
     
     //primeiro questionario/Carta1
     printf("Carta1! \n\n");
+
 
     printf("Digite o Estado: ");
     scanf("%9s", Estado);
@@ -78,15 +79,15 @@ int main() {
     scanf("%d", &Ponto2);
 
 
-    //Calculo
+    //Calculo                                                                   //variaveis calculos adicionads
     densidadePopulacao = (Area != 0) ? (float)Populacao / Area : 0.0f;
     densidadePopulacao2 = (Area2 != 0) ? (float) Populacao2 / Area2 : 0.0f;
 
     pibTotal = PIB * 1000000000.0f;
     pibTotal2 = PIB2 * 1000000000.0f;
 
-    pibpercapita = (Populacao != 0) ? pibTotal / Populacao : 0.0f;
-    pibpercapita2 = (Populacao2 != 0) ? pibTotal2 / Populacao2 : 0.0f;
+    pibpercapita = (Populacao != 0) ? pibTotal / (float)Populacao : 0.0f;
+    pibpercapita2 = (Populacao2 != 0) ? pibTotal2 / (float)Populacao2 : 0.0f;
 
     superPoder1 = (float)Populacao + Area + pibTotal + (float)Ponto + pibpercapita + densidadePopulacao;
     superPoder2 = (float)Populacao2 +Area2 + pibTotal2 + (float)Ponto2 +pibpercapita2 + densidadePopulacao2;
@@ -94,8 +95,8 @@ int main() {
 
 
      //comparação 
-  printf("\n--- Comparação ---\n");
-  printf("Populacao: carta 1 venceu (%lu)\n", Populacao > Populacao2);
+  printf("\n--- Comparação ---\n");                                                //comparação de resultados
+  printf("Populacao: carta 1 venceu (%.lu)\n", Populacao > Populacao2);
   printf("area: carta 1 venceu(%d)\n", Area > Area2);
   printf("pib: carta 1 venceu (%d)\n", pibTotal > pibTotal2);
   printf("pontos turisticos: carta 1 venceu (%d)\n", Ponto > Ponto2);
@@ -103,7 +104,6 @@ int main() {
   printf("pib per capita: carta 1 venceu (%d)\n", pibpercapita > pibpercapita2);
   printf("super poder: carta 1 venceu (%d)\n", superPoder1 > superPoder2);
 
-  
 
     printf("\n--- CIDADE 1---\n");
 
@@ -112,9 +112,9 @@ int main() {
     printf("Codigo: %s\n", Codigo);
     printf("Populacao: %lu\n", Populacao);
     printf("Area (m²): %.2f\n", Area);
-    printf("PIB: %f\n", pibTotal);
+    printf("PIB: %.2f\n", pibTotal);
     printf("Pontos Turisticos: %d\n", Ponto);
-    printf("Densidade Populacional: %f hab/km²\n", densidadePopulacao); //variaveis float adicionadas para impressão dos resultados.
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacao); //variaveis float adicionadas para impressão dos resultados.
     printf("PIB Per Capita: %.2f reais\n", pibpercapita);
 
      printf("\n--- Cidade2 ---\n");
@@ -122,22 +122,33 @@ int main() {
     printf("Cidade: %s\n", Cidade2);
     printf("Código: %s\n", Codigo2);
     printf("População: %lu\n", Populacao2);
+    
     printf("Área (m²): %.2f\n", Area2);
     printf("PIB: %f\n", pibTotal);
     printf("Pontos Turísticos: %d\n", Ponto2);
-    printf("Densidade Populacional: %f hab/km²\n", densidadePopulacao2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacao2);
     printf("PIB Per Capita: %.2f reais\n", pibpercapita2);
-
-      //impressão de calculos
+ 
+      //impressão de calculos                                                         //novas impressoes de calculos adicionadas
    printf("\n--- Resultados ---\n");
    printf("carta 1 - desnsidade populacional: %.2f hab/km²\n", densidadePopulacao);
    printf("carta 1 - pib: R$ %f\n", pibTotal);
    printf("carta1 - super poder: %.2f\n", superPoder1);
 
-   printf("\ncarta 2 - desnsidade populacional: %f\n", densidadePopulacao2);
+   printf("\ncarta 2 - desnsidade populacional: %.2f\n", densidadePopulacao2);
    printf("carta 2 - pib: R$ %f\n", pibTotal2);
    printf("carta 2 - super poder: %2.f\n", superPoder2);
 
+//comparação (if_else)                                                                    //variaveis: if_else adicionadas
+    printf("\n--- Comparação (população) ---\n");
+    printf("carta 1 - %s (%s): %d População\n", Cidade, Estado, densidadePopulacao);
+    printf("carta 2 - %s (%s): %d População\n", Cidade2, Estado2, densidadePopulacao2); 
+
+    if(Populacao > Populacao2){
+      printf("\nresultado: carta 1 (%s) venceu!\n", Cidade);
+   }else{
+    printf("\nresultado: carta 2 (%s) venceu!\n", Cidade2);
+   }
 
    
 
